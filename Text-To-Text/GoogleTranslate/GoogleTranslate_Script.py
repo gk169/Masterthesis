eaf94743171b9acd9e9d6fa8fba39c5766429bb4
AI-RYZEN1 <pascal.graf@hs-heilbronn.de>
2021-04-13 22:57:19 +0200

    Add time measurement

--- Text-To-Text/GoogleTranslate/GoogleTranslate_Script.py
+++ Text-To-Text/GoogleTranslate/GoogleTranslate_Script.py
@@ -3,12 +3,18 @@
 import requests
 import urllib
 import sys
+from time import time
 
 sourceLangCode = sys.argv[1]    #de
 targetLangCode = sys.argv[2]    #en
 
-input_text = sys.argv[3]        #"text"
+input_path = sys.argv[3]        #.txt
 output_path = sys.argv[4]       #.txt
+
+tic_all = time()
+f = open(input_path,"r", encoding="utf-8")
+input_text = f.read()
+f.close()
 
 text = urllib.parse.quote(input_text)
 
@@ -30,3 +36,9 @@ translated_text = translated_text.replace(u"\u200B","")
 f = open(output_path,"w", encoding="utf-8")
 f.write(translated_text)
 f.close()
+
+toc_all = time()
+
+print("All took: %f" % (toc_all - tic_all))
+
+input()#"Enter to end")
