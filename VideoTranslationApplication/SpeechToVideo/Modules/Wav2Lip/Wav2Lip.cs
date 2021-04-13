cf250137b97d22db47fb380feed2f053f9cba799
AI-RYZEN1 <pascal.graf@hs-heilbronn.de>
2021-04-13 23:04:37 +0200

    Add file conversation to wav2lip

--- VideoTranslationApplication/SpeechToVideo/Modules/Wav2Lip/Wav2Lip.cs
+++ VideoTranslationApplication/SpeechToVideo/Modules/Wav2Lip/Wav2Lip.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.Composition;
 using System.Diagnostics;
 using System.IO;
+using VideoTranslationTool.FileUtils;
 
 namespace VideoTranslationTool.SpeechToVideoModule
 {
@@ -31,23 +32,51 @@ namespace VideoTranslationTool.SpeechToVideoModule
         /// <returns></returns>
         public override string Generate(string inputAudioPath, string inputVideoPath)
         {
+            #region Inputs
             // Provide arguments
             string outputVideoPath_Windows = Path.GetTempPath() + "GeneratedVideo.mp4";
+            string wav2lipWeightPath = @"E:\206309_Gann_Kevin\weights\Wav2Lip\wav2lip.pth";
+
+            // If file is mp3 -> convert to wav
+            if (Path.GetExtension(inputAudioPath) == ".mp3")
+            {
+                string audioPath_wav = Path.GetTempPath() + "ToTranscribe.wav";
+                AudioConverter.Mp3ToWav(inputAudioPath, audioPath_wav);
+                inputAudioPath = audioPath_wav;
+            }
 
             // Transform paths https://www.btelligent.com/blog/best-practice-arbeiten-in-python-mit-pfaden-teil-1/
             string inputAudioPath_Unix = inputAudioPath.Replace(@"\", "/");
             string inputVideoPath_Unix = inputVideoPath.Replace(@"\", "/");
+            string wav2lipWeightPath_Unix = wav2lipWeightPath.Replace(@"\", "/");
             string outputVideoPath_Unix = outputVideoPath_Windows.Replace(@"\", "/");
+            #endregion Inputs
 
-            // Provide executable
+            #region Process
+            /* arguments
+             * 1: Wav2LipWeightPath
+             * 2: InputVideoPath
+             * 3: InputAudioPath
+             * 4: OutputVideoPath
+             */
+            #region Option 1: Python script
 
-            string executable = @"C:\ProgramData\Anaconda3\python.exe"; // Note: if env is used, the env python has to be here!
-            //string executable = @"C:\ProgramData\Anaconda3\envs\TTS\python.exe";
-            string script = @"D:\GitRepos\Masterthesis\git\Text-To-Text-Translation\Script_GoogleTranslatorApi.py";
-            string arguments = $"\"{script}\" \"{inputAudioPath_Unix}\" \"{inputVideoPath_Unix}\" \"{outputVideoPath_Unix}\"";
+            string executable = "cmd.exe";
+
+            string script = @"E:\206309_Gann_Kevin\git\Speech-To-Lip\Wav2Lip_Script.py";
+
+            string wav2lipArguments = $"\"{script}\" \"{wav2lipWeightPath_Unix}\" \"{inputVideoPath_Unix}\" \"{inputAudioPath_Unix}\" \"{outputVideoPath_Unix}\"";
+
+            string arguments = "/c " + @"C:\ProgramData\Anaconda3\Scripts\activate.bat" + "&&" + "activate Wav2Lip" + "&&" + "python " + wav2lipArguments;
+
+            #endregion Option 1: Python script
+
+            #region Option 2: Executable
 
             //string executable = @"D:\GitRepos\Masterthesis\git\Text-To-Text-Translation\dist\Script_GoogleTranslatorApi.exe";
             //string arguments = $"\"{inputAudioPath_Unix}\" \"{inputVideoPath_Unix}\" \"{outputVideoPath_Unix}\"";
+
+            #endregion Option 2: Executable
 
             // Create process info
             ProcessStartInfo processStartInfo = new()
@@ -55,7 +84,7 @@ namespace VideoTranslationTool.SpeechToVideoModule
                 FileName = executable,
                 Arguments = arguments,
                 UseShellExecute = false,
-                CreateNoWindow = true,
+                CreateNoWindow = false,
                 RedirectStandardError = true,
             };
 
@@ -63,9 +92,13 @@ namespace VideoTranslationTool.SpeechToVideoModule
             string errors = "";
             using (Process process = Process.Start(processStartInfo)) { errors = process.StandardError.ReadToEnd(); }
 
+            #endregion Process
+
+            #region Outputs
             // Handle errors and outputs
-            if (errors != "") throw new Exception(errors);
+            if (errors.Contains("Error")) throw new Exception(errors);
             else return outputVideoPath_Windows;
+            #endregion Outputs
         }
         #endregion Methods
     }
